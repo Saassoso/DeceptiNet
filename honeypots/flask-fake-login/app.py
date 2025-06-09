3cab95643ce832525a825d899ec9a02d3299e8cf
Saassoso <charifsaad38@gmail.com>
2025-06-09 17:28:01 +0100

    --day4
    changed some docker compose conf , and also the fake pake loggoing solved the dionnea problem

--- honeypots/flask-fake-login/app.py
+++ honeypots/flask-fake-login/app.py
@@ -1,20 +1,26 @@
-from flask import Flask, request
-from datetime import datetime
+from flask import Flask, request, render_template_string
+import logging
 
 app = Flask(__name__)
+logging.basicConfig(filename='login.log', level=logging.INFO)
 
-@app.route('/login', methods=['GET', 'POST'])
+HTML = '''
+<form method="POST">
+  <input type="text" name="username" placeholder="Username"/><br/>
+  <input type="password" name="password" placeholder="Password"/><br/>
+  <input type="submit" value="Login"/>
+</form>
+'''
+
+@app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        with open('login_attempts.log', 'a') as f:
-            f.write(f"{datetime.now()} - USER: {request.form.get('username')} | PASS: {request.form.get('password')}\n")
-        return "Invalid credentials"
-    return '''
-        <form method="POST">
-            Username: <input name="username"><br>
-            Password: <input name="password" type="password"><br>
-            <input type="submit" value="Login">
-        </form>
-    '''
+        ip = request.remote_addr
+        ua = request.headers.get('User-Agent')
+        username = request.form['username']
+        password = request.form['password']
+        logging.info(f"Login attempt from {ip}, UA: {ua}, Username: {username}, Password: {password}")
+    return render_template_string(HTML)
 
-app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
